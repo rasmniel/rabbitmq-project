5d04b9a1f09167036b999ee93a71762090ecc9ff
hardydrachmann <hardydrachmann@phoebuscartel.dk>
2017-10-16 10:01:44 +0200

    Test

---
+++ Messages/TextMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Messages
+{
+    public class TextMessage
+    {
+        public string Product { get; set; }
+        public string Country { get; set; }
+        public string Sender { get; set; }
+    }
+}
